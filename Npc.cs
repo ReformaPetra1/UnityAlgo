4c2c2f512accaeebc8dbc6fe1b58c1608a55da36
ReformaPetra1 <dedqwq00@gmail.com>
2024-12-28 17:00:16 +0800

    Add files via upload

---
+++ Npc.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Npc : MonoBehaviour
+{   
+    //Здоровье Npc
+    public int health = 5;
+
+    //Уровень Npc
+    public int level = 1;
+
+    //Скорость Npc
+    public float speed = 1.2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+              
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
